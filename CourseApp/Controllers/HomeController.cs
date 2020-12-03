5acae948599033d5d78a61c41298c157173c30ef
Busra <Busra@BUSRALAPTOP>
2020-12-03 14:14:46 +0300

    Add project files.

---
+++ CourseApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            CourseApp.Models.Request model = new Models.Request();
+            model.Name = "Büşra Özmen";
+            model.Phone = "012344556789";
+            model.Email = "busraozmen@outlook.com";
+            model.Message = "Limon bahçesi yapacağım.";
+            return View(model);
+
+        }
+    }
+}
